1133c1df3bfa697dfd8959a6fe540f7a87784108
cameronduffin <camduffin@gmail.com>
2021-03-31 11:31:06 -0400

    playing with smooth scroll and scroll reveal

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: "isilive-test",
   },
   plugins: [
+    "gatsby-plugin-smoothscroll",
     "gatsby-plugin-scroll-reveal",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
@@ -15,13 +16,14 @@ module.exports = {
       __key: "images",
     },
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-nprogress",
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
         // Setting a color is optional.
         color: `tomato`,
         // Disable the loading spinner.
-        showSpinner: false,
+        showSpinner: true,
       },
     },
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import * as React from "react"
 import '../../styles/index.scss';
 import Layout from '../components/Layout';
-import { StaticImage } from 'gatsby-plugin-image';
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 const IndexPage = () => {
 
   return (
       <Layout>
-        <div className="main-image-header" data-sal="fade-in">
+        <div className="main-image-header" data-sal="fade-in" data-sal-easing="ease" data-sal-delay="100">
           <h1 className='about-header'>YESSSSSSs</h1>
+          <button onClick={() => scrollTo('#content')}>Scroll</button>
         </div>
       
-        <div className="wrapper">
+        <div className="wrapper" id="content">
           <p>We are here to help you turn your day into ourday!</p>
           <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat nihil reprehenderit voluptatibus odio quam iusto totam labore velit quae exercitationem doloremque ab ipsa quasi suscipit officia est dolor et unde, voluptates deserunt tempora ducimus reiciendis, blanditiis eligendi. Qui nihil illo blanditiis suscipit exercitationem rem deserunt quae, nobis voluptates placeat eveniet earum dolores, beatae eum, accusamus quam veniam nam incidunt aliquam vitae. Optio minima, qui doloremque consequuntur eius illo in tempora beatae, nesciunt cupiditate repellat cum atque, est ad ea culpa veniam assumenda voluptate dolores aliquam doloribus laboriosam voluptas vitae quaerat? Iusto voluptates, vero aspernatur unde illo molestiae vel recusandae ut!</p>
         </div>
