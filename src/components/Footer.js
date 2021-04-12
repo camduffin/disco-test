11bcce4a7c4d544897147b3afd67bf6ce7de4c51
cameronduffin <camduffin@gmail.com>
2021-04-12 15:30:37 -0400

    media queries round 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,22 +6,22 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-            <div className="address">
-                <p>150 C Terence</p>
-                <p>Matthews Cres</p>
-                <p>Ottawa, ON K2M1X4</p>
-            </div>
-            <span aria-hidden="true"></span>
-            <div className="phone">
-                <p>Tel: 613.737.3378</p>
-                <a href="mailto:info@ourday.ca">info@ourday.ca</a>
-            </div>
-            <span aria-hidden="true"></span>
-            <div className="copyright">
-                <p>© 2021 by OurDay.ca</p>
-                <p>Presented By</p>
-                <a href="https://isilive.ca/" target="_blank" rel="noreferrer noopener">www.isilive.ca</a>
-            </div>
+                <div className="address">
+                    <p>150 C Terence</p>
+                    <p>Matthews Cres</p>
+                    <p>Ottawa, ON K2M1X4</p>
+                </div>
+                <span aria-hidden="true"></span>
+                <div className="phone">
+                    <p>Tel: 613.737.3378</p>
+                    <a href="mailto:info@ourday.ca">info@ourday.ca</a>
+                </div>
+                <span aria-hidden="true"></span>
+                <div className="copyright">
+                    <p>© 2021 by OurDay.ca</p>
+                    <p>Presented By</p>
+                    <a href="https://isilive.ca/" target="_blank" rel="noreferrer noopener">www.isilive.ca</a>
+                </div>
                 <nav>
                     <SocialLinks />
                 </nav>
