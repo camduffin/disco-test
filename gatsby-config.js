2665a6b2b01f081ef6d730d09604f170291f2d59
cameronduffin <camduffin@gmail.com>
2021-04-13 12:51:31 -0400

    back to original file path

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/static/`,
+        path: `${__dirname}/src/images/`,
       },
       __key: `images`,
     },
