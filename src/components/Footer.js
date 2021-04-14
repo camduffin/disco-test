4a226182a9846abefae4c3cc0650f3b44f957760
cameronduffin <camduffin@gmail.com>
2021-04-14 11:43:01 -0400

    adding in iSiLIVE logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SocialLinks from './SocialLinks';
 import '../../styles/index.scss';
+import logo from '../images/isi-logo.png';
 
 const Footer = () => {
     return (
@@ -12,19 +13,23 @@ const Footer = () => {
                     <p>Ottawa, ON K2M1X4</p>
                 </div>
                 <span aria-hidden="true"></span>
+                <div className="copyright">
+                    <p>Presented By</p>
+                    <a href="https://isilive.ca/" target="_blank" rel="noreferrer noopener">
+                        <img className="isi-logo" src={logo} alt="iSiLIVE logo"/>
+                    </a>
+                </div>
                 <div className="phone">
                     <a href="tel:613.737.3378">Tel: 613.737.3378</a>
                     <a href="mailto:info@ourday.ca">info@ourday.ca</a>
                 </div>
                 <span aria-hidden="true"></span>
-                <div className="copyright">
-                    <p>Presented By</p>
-                    <a href="https://isilive.ca/" target="_blank" rel="noreferrer noopener">www.isilive.ca</a>
-                    <p>© 2021 by OurDay.ca</p>
-                </div>
                 <nav>
                     <SocialLinks />
                 </nav>
+            </div>
+            <div className="copyright-bottom">
+                <p>© 2021 OurDay.ca</p>
             </div>
         </footer>
     )
